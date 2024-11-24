58a2ad9b2530c3b266b4c1105ee2dcabde158901
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-11-24 10:34:23 +0800

    refacto: display username instead of user.name

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,42 @@
 "use client";
 
+import { useProfile } from "@/hooks/useProfile";
 import { useUser } from "@/hooks/useUser";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   // TODO: Create a useNavigation hook to handle useRouter and other navigation-related logic
   const router = useRouter();
+  const { getProfile } = useProfile();
+  const [profile, setProfile] = useState<any>(null);
   const { user, loading } = useUser();
+  const [userId, setUserId] = useState<string | null>(null);
 
-  if (loading) {
+  const fetchProfile = async (userId: string) => {
+    try {
+      const profile = await getProfile(userId);
+      setProfile(profile);
+    } catch (error: any) {
+      console.error("Error fetching profile:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (!loading && user?.id && !userId) {
+      fetchProfile(user.id);
+      setUserId(user.id);
+    }
+  }, [user, loading]);
+
+  if (loading || !user || !profile) {
     return <p>Loading user information...</p>;
   }
 
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <h1 className="text-2xl font-semibold">
-        Welcome back, {user.user_metadata?.firstname}
+        Welcome back, {profile.username}!
       </h1>
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
